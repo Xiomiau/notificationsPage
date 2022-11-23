a26845df04eb620595320e51c2de1987505bfb93
Xiomiau <xiomi_arzola@outlook.com>
2022-11-23 16:14:41 -0700

    finish

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const jacob = document.querySelector(".not-jacob");
 const dotm  = document.querySelector(".m");
 const dota  = document.querySelector(".a");
 const dotj  = document.querySelector(".j");
-const counter =document.querySelector("#number")
+const counter =document.querySelector("#number");
+const markAll =document.querySelector("#markAll");
+const notification =document.querySelector(".notificactionUnread");
+const dot = document.querySelector(".redDot")
 contador = 3;
 
 mark.onclick = function () {
@@ -25,6 +28,15 @@ jacob.onclick = function () {
     contador--;
     counter.textContent=contador;
     jacob.style.backgroundColor = 'white';
+    dotj.style.display= 'none'
+}
+
+markAll.onclick = function () {
+    mark.style.backgroundColor = 'white';
+    angela.style.backgroundColor = 'white';
+    jacob.style.backgroundColor = 'white';
+    dotm.style.display= 'none'
+    dota.style.display= 'none'
     dotj.style.display= 'none'
 }
 
